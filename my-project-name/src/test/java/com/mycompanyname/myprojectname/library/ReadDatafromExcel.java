52a7c5830e5d02b0be456ed100bc2affd4b04048
Wadageri, Susheelkumar <wadageri.susheelkumar@gmail.com>
2017-08-23 10:42:19 +0530

    refractoring the project 2

--- my-project-name/src/test/java/com/mycompanyname/myprojectname/library/ReadDatafromExcel.java
+++
@@ -1,5 +0,0 @@
-package com.mycompanyname.myprojectname.library;
-
-public class ReadDatafromExcel {
-
-}
--- my-project-name/src/test/java/com/mycompanyname/myprojectname/tests/BaseClass.java
+++
@@ -1,5 +0,0 @@
-package com.mycompanyname.myprojectname.tests;
-
-public class BaseClass {
-
-}
